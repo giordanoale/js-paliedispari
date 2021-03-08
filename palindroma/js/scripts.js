3364edbbe73e06bb48e20f4378f79f867ffcc52e
giordanoale <a.giordano89@gmail.com>
2021-03-08 17:23:35 +0100

    Il programma Pari&Dispari funziona

--- palindroma/js/scripts.js
+++ palindroma/js/scripts.js
@@ -1,2 +1,14 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+function palindroma (parola) {
+  for ( var i = 0; i < parola.length; i++) {
+    console.log(parola[i]);
+  }
+  for ( var j = parola.length; j < 1; j--) {
+    console.log(j);
+    console.log(parola.length);
+  }
+}
+
+var utente = palindroma(prompt("Inserisci una parola"));
--- pari e dispari/js/scripts.js
+++ pari e dispari/js/scripts.js
@@ -1,1 +1,58 @@
-alert();
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+var utente;
+
+var numero1 = 0;
+
+while (utente != "pari" && utente != "dispari") {
+  utente = prompt("Scegli se pari o dispari");
+  utente = utente.toLowerCase();
+}
+
+console.log(utente);
+
+numero1 = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+
+
+console.log(numero1);
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+function random (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var numero2 = random(1,5);
+
+console.log(numero2);
+
+// Sommiamo i due numeri
+
+var somma = numero1 + numero2;
+
+console.log(somma);
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+function ifEven (num) {
+  if (num % 2 == 0) {
+    return "pari";
+  } else {
+    return "dispari";
+  }
+}
+
+console.log(ifEven(somma));
+
+var risultato = ifEven(somma);
+
+// Dichiariamo chi ha vinto.
+
+if (utente == risultato) {
+  console.log("Hai vinto!");
+} else {
+  console.log("Ha vinto il computer!");
+}
